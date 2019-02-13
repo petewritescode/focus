ac9882963594abc726b822f826ba47b157c35771
Pete Williams <hello@petewritescode.com>
2019-02-13 18:11:36 +0000

    Display site name and block status in block button
    If the protocol can be blocked, show the site name and the current block status. If it can't be blocked, show "Unavailable" and disable the button.

--- background.js
+++ background.js
@@ -7,6 +7,17 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.message) {
+        case 'getUrlDetails':
+            chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+                const url = new URL(tabs[0].url);
+
+                sendResponse({
+                    protocol: url.protocol,
+                    hostname: url.hostname,
+                });
+            });
+            return ASYNC_SEND_RESPONSE_RETURN_VALUE;
+
         case 'enable':
         case 'disable':
             chrome.storage.local.set({ enabled: request.message === 'enable' }, () => {
--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+const BLOCKABLE_PROTOCOLS = [ 'http:', 'https:' ];
 const STATUS_BUTTON_ACTIVE_CLASS = 'status__button--active';
 
 const enableButton = document.getElementById('enableButton');
@@ -13,6 +14,20 @@ const updateStatusButtons = () => {
             enableButton.classList.add(STATUS_BUTTON_ACTIVE_CLASS);
         } else {
             disableButton.classList.add(STATUS_BUTTON_ACTIVE_CLASS);
+        }
+    });
+}
+
+const updateBlockButton = () => {
+    chrome.runtime.sendMessage({ message: 'getUrlDetails'}, ({ protocol, hostname }) => {
+        if (BLOCKABLE_PROTOCOLS.includes(protocol)) {
+            chrome.storage.sync.get('sites', ({ sites }) => {
+                const blocked = sites.includes(hostname);
+
+                blockButton.innerHTML = `${blocked ? 'Unblock' : 'Block'} ${hostname}`;
+                blockButton.value = hostname;
+                blockButton.removeAttribute('disabled');
+            });
         }
     });
 }
@@ -34,3 +49,4 @@ blockButton.addEventListener('click', () => {
 });
 
 updateStatusButtons();
+updateBlockButton();
